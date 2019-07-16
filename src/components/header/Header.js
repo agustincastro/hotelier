2cad213436dedf52fe5a2e4d4de5e542ecd82e6c
Agustin Castro <agustin.castro.91@gmail.com>
2019-07-16 22:17:10 +0200

    Improved icons in navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,6 +10,9 @@ import {
     Input,
     Button
 } from 'reactstrap';
+import { faHeart, faPaperPlane, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import logo from "../../assets/images/logos/logo.png";
 import "./Header.css";
 
@@ -42,27 +45,37 @@ export default class Header extends React.Component {
 
                         <Nav id="navbar-group-left" className="ml-auto" navbar>
                             <NavItem>
-                                <Input type="text" name="navbarSearch" id="navbar-search" placeholder="Search a city..." />
+                                <Input type="text" name="navbarSearch" id="navbar-search" placeholder="Search a city..." >
+                                </Input>
                             </NavItem>
 
                         </Nav>
 
-                        <Nav className="ml-auto" navbar>
+                        <Nav className="ml-auto navbar-upper-right" navbar>
                             <NavItem>
-                                <NavLink href="/favorites/">Favorites</NavLink>
+                                <NavLink href="/favorites/">
+                                    <FontAwesomeIcon icon={faHeart} color="rgba(0,0,0,.5)" className="navbar-link-icon" />
+                                    Favorites
+                                </NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink href="/trips/">My trips</NavLink>
+                                <NavLink href="/trips/">
+                                    <FontAwesomeIcon icon={faPaperPlane} color="rgba(0,0,0,.5)" className="navbar-link-icon" />
+                                    My trips
+                                </NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink href="/about/">About</NavLink>
+                                <NavLink href="/about/">
+                                    <FontAwesomeIcon icon={faUserCircle} color="rgba(0,0,0,.5)" className="navbar-link-icon" />
+                                    About
+                                </NavLink>
                             </NavItem>
                         </Nav>
                     </Collapse>
                 </Navbar>
 
 
-                <Navbar color="white" id="second-header-nav"className="header-nav" light expand="md">
+                <Navbar color="white" id="second-header-nav" className="header-nav" light expand="md">
                     <Nav id="lower-navbar-group-left" className="ml-auto" navbar>
                         <NavItem>
                             <Button color="secondary" className="btn-xs btn-white">Dates</Button>
